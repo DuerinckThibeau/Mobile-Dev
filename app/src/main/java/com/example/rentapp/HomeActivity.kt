ad68645d2ed6d8d0765d0507c216c98e662d5153
Thibeau Duerinck <thibeau.duerinck@icloud.com>
2024-11-13 11:01:51 +0100

    Add basic logout button to homepage
    To test functionality

--- app/src/main/java/com/example/rentapp/HomeActivity.kt
+++ app/src/main/java/com/example/rentapp/HomeActivity.kt
@@ -1,11 +1,27 @@
 package com.example.rentapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        auth = FirebaseAuth.getInstance()
+
+        findViewById<Button>(R.id.logoutButton).setOnClickListener {
+            auth.signOut()
+            // Navigate back to login screen
+            val intent = Intent(this, LoginActivity::class.java)
+            // Clear the back stack so user can't go back after logging out
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
     }
 } 
